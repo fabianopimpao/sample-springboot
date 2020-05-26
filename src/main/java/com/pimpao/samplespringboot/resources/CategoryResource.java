8d4e2242a610f36bce9752c8be56580401df7bc7
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-05-26 07:34:58 -0300

    Created the first domain class category

--- src/main/java/com/pimpao/samplespringboot/resources/CategoryResource.java
+++ src/main/java/com/pimpao/samplespringboot/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.pimpao.samplespringboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pimpao.samplespringboot.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is avaiable";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<Category>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
