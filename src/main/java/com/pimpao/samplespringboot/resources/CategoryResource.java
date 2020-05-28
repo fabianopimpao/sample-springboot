fe9fb6734811a50378f6c82ccc0c8cea438dd303
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-05-28 07:12:34 -0300

    Creating repository and service for Category

--- src/main/java/com/pimpao/samplespringboot/resources/CategoryResource.java
+++ src/main/java/com/pimpao/samplespringboot/resources/CategoryResource.java
@@ -1,27 +1,25 @@
 package com.pimpao.samplespringboot.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pimpao.samplespringboot.domain.Category;
+import com.pimpao.samplespringboot.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Category> list() {
-		Category cat1 = new Category(1, "Informática");
-		Category cat2 = new Category(2, "Escritório");
-		
-		List<Category> list = new ArrayList<Category>();
-		list.add(cat1);
-		list.add(cat2);
-		
-		return list;
+	@Autowired
+	private CategoryService categoryService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable("id") Integer id) {
+		Category category = categoryService.findById(id);
+		return ResponseEntity.ok().body(category);
 	}
 }
