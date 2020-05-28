6e172f602fa3ad5a08983870b0bbedd4ec4ef0ee
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-05-28 07:26:05 -0300

    Creating instance operation

--- src/main/java/com/pimpao/samplespringboot/SampleSpringbootApplication.java
+++ src/main/java/com/pimpao/samplespringboot/SampleSpringbootApplication.java
@@ -1,13 +1,31 @@
 package com.pimpao.samplespringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SampleSpringbootApplication {
+import com.pimpao.samplespringboot.domain.Category;
+import com.pimpao.samplespringboot.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class SampleSpringbootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRespository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRespository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
